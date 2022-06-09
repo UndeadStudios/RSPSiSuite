f9f56beb035bc958119c9da693d1f3478ca87f4c
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 11:43:49 -0700

    Initial commit

---
+++ Client/src/main/java/com/jagex/entity/model/MeshRevision.java
@@ -0,0 +1,5 @@
+package com.jagex.entity.model;
+
+public enum MeshRevision {
+	OLD_FORMAT, TYPE_1, TYPE_2, TYPE_3
+}
---
+++ Client/src/main/java/com/jagex/entity/model/PreviewModel.java
@@ -0,0 +1,53 @@
+package com.jagex.entity.model;
+
+public class PreviewModel extends Mesh {
+
+	public PreviewModel(Mesh model) {
+		super();
+		fitsOnSingleSquare = (model.fitsOnSingleSquare);
+		minimumX = (model.minimumX);
+		maximumX = (model.maximumX);
+		maximumZ = (model.maximumZ);
+		minimumZ = (model.minimumZ);
+		boundingPlaneRadius = (model.boundingPlaneRadius);
+		minimumY = (model.minimumY);
+		boundingSphereRadius = (model.boundingSphereRadius);
+		boundingCylinderRadius = (model.boundingCylinderRadius);
+		anInt1654 = (model.anInt1654);
+		shadedFaceColoursX = copyArray(model.shadedFaceColoursX);
+		shadedFaceColoursY = copyArray(model.shadedFaceColoursY);
+		shadedFaceColoursZ = copyArray(model.shadedFaceColoursZ);
+		faceTransparencies = copyArray(model.faceTransparencies);
+		triangleColors = copyArray(model.triangleColors);
+		faceMaterial = copyArray(model.faceMaterial);
+		faceTexture = copyArray(model.faceTexture);
+		textureMap = copyArray(model.textureMap);
+		faceGroups = copyArray(model.faceGroups);
+		faceRenderPriorities = copyArray(model.faceRenderPriorities);
+		triangleCount = (model.triangleCount);
+		packedTransparencyVertexGroups = copyArray(model.packedTransparencyVertexGroups);
+		faceIndices1 = copyArray(model.faceIndices1);
+		faceIndices2 = copyArray(model.faceIndices2);
+		faceIndices3 = copyArray(model.faceIndices3);
+		normals = (model.normals);
+		priority = (model.priority);
+		numTextureFaces = model.numTextureFaces;
+		texIndices1 = copyArray(model.texIndices1);
+		texIndices2 = copyArray(model.texIndices2);
+		texIndices3 = copyArray(model.texIndices3);
+		triangleInfo = copyArray(model.triangleInfo);
+		vertexGroups = copyArray(model.vertexGroups);
+		packedVertexGroups = copyArray(model.packedVertexGroups);
+		vertexX = copyArray(model.vertexX);
+		vertexY = copyArray(model.vertexY);
+		vertexZ = copyArray(model.vertexZ);
+		vertexCount = model.vertexCount;
+
+	}
+
+	@Override
+	public int getZVertexMax() {
+		return 500;
+	}
+
+}
